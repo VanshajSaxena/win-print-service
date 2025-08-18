eba667f55320ee136e1974a90130bcf2d6152ab0
Vanshaj Saxena <vs110405@outlook.com>
2025-08-18 15:33:35 +0530

    fix: use dto to serialize data

--- PrintService/Models/PrintQueueDto.cs
+++ PrintService/Models/PrintQueueDto.cs
@@ -21,13 +21,13 @@ namespace PrintService.Models
         public List<string>? OutputColor { get; set; }
         public List<string>? OutputQuality { get; set; }
         public List<string>? PageBorderless { get; set; }
-        public Dictionary<string, double?>? PageImageableArea { get; set; }
-        public List<string>? PageMediaSize { get; set; }
+        public PageImageableAreaDto? PageImageableArea { get; set; }
+        public List<PageMediaSizeDto>? PageMediaSize { get; set; }
         public List<string>? PageMediaType { get; set; }
         public List<string>? PageOrder { get; set; }
         public List<string>? PageOrientation { get; set; }
-        public List<string>? PageResolution { get; set; }
-        public Dictionary<string, int?>? PageScalingFactorRange { get; set; }
+        public List<PageResolutionDto>? PageResolution { get; set; }
+        public PageScalingFactorRangeDto? PageScalingFactorRange { get; set; }
         public List<string>? PagesPerSheet { get; set; }
         public List<string>? PagesPerSheetDirection { get; set; }
         public List<string>? PhotoPrintingIntent { get; set; }
@@ -35,4 +35,31 @@ namespace PrintService.Models
         public List<string>? TrueTypeFontMode { get; set; }
     }
 
+    public record PageImageableAreaDto
+    {
+        public double? ExtentHeight { get; set; }
+        public double? ExtentWidth { get; set; }
+        public double? OriginHeight { get; set; }
+        public double? OriginWidth { get; set; }
+    }
+
+    public record PageMediaSizeDto
+    {
+        public string? SizeName { get; set; }
+        public string? Height { get; set; }
+        public string? Width { get; set; }
+    }
+
+    public record PageResolutionDto
+    {
+        public string? Resolution { get; set; }
+        public string? X { get; set; }
+        public string? Y { get; set; }
+    }
+
+    public record PageScalingFactorRangeDto
+    {
+        public int? MaximumScale { get; set; }
+        public int? MinimumScale { get; set; }
+    }
 }
--- PrintService/Services/PrintQueueService.cs
+++ PrintService/Services/PrintQueueService.cs
@@ -24,7 +24,7 @@ namespace PrintService.Services
 
         public async Task<PrintQueueDto> GetPrintQueue(string queueName)
         {
-            _logger.LogInformation("{MethodName} was invoked", nameof(GetPrintQueues));
+            _logger.LogInformation("{MethodName} was invoked with parameter: {queueName}", nameof(GetPrintQueueCapabilities), queueName);
             return await Application.Current.Dispatcher.InvokeAsync(() =>
             {
                 using var server = new LocalPrintServer();
@@ -46,7 +46,7 @@ namespace PrintService.Services
 
         public async Task<PrintCapabilitiesDto> GetPrintQueueCapabilities(string queueName)
         {
-            _logger.LogInformation("{MethodName} was invoked", nameof(GetPrintQueueCapabilities));
+            _logger.LogInformation("{MethodName} was invoked with parameter: {queueName}", nameof(GetPrintQueueCapabilities), queueName);
             return await Application.Current.Dispatcher.InvokeAsync(() =>
             {
                 using var server = new LocalPrintServer();
@@ -56,7 +56,7 @@ namespace PrintService.Services
                 });
                 PrintCapabilities capabilities = current.GetPrintCapabilities();
 
-                PrintCapabilitiesDto dto = new PrintCapabilitiesDto() with
+                PrintCapabilitiesDto dto = new()
                 {
                     Collation = [.. capabilities.CollationCapability.Select(c => c.ToString())],
                     DeviceFontSubstitution = [.. capabilities.DeviceFontSubstitutionCapability.Select(c => c.ToString())],
@@ -68,20 +68,32 @@ namespace PrintService.Services
                     OutputColor = [.. capabilities.OutputColorCapability.Select(c => c.ToString())],
                     OutputQuality = [.. capabilities.OutputQualityCapability.Select(c => c.ToString())],
                     PageBorderless = [.. capabilities.PageBorderlessCapability.Select(c => c.ToString())],
-                    PageImageableArea = new Dictionary<string, double?> {
-                        { "ExtentHeight", capabilities.PageImageableArea.ExtentHeight },
-                        { "ExtentWidth", capabilities.PageImageableArea.ExtentWidth },
-                        { "OriginHeight", capabilities.PageImageableArea.OriginHeight },
-                        { "OriginWidth", capabilities.PageImageableArea.OriginWidth },
+                    PageImageableArea = new PageImageableAreaDto
+                    {
+                        ExtentHeight = capabilities.PageImageableArea.ExtentHeight,
+                        ExtentWidth = capabilities.PageImageableArea.ExtentWidth,
+                        OriginHeight = capabilities.PageImageableArea.OriginHeight,
+                        OriginWidth = capabilities.PageImageableArea.OriginWidth,
                     },
-                    PageMediaSize = [.. capabilities.PageMediaSizeCapability.Select(c => c.ToString())], // TODO: improve serialization
+                    PageMediaSize = [.. capabilities.PageMediaSizeCapability.Select(c => new PageMediaSizeDto
+                    {
+                        SizeName = c.PageMediaSizeName.ToString(),
+                        Height = c.Height.ToString(),
+                        Width = c.Width.ToString(),
+                    })],
                     PageMediaType = [.. capabilities.PageMediaTypeCapability.Select(c => c.ToString())],
                     PageOrder = [.. capabilities.PageOrderCapability.Select(c => c.ToString())],
                     PageOrientation = [.. capabilities.PageOrientationCapability.Select(c => c.ToString())],
-                    PageResolution = [.. capabilities.PageResolutionCapability.Select(c => c.ToString())], // TODO: improve serializatio;
-                    PageScalingFactorRange = new Dictionary<string, int?> {
-                        {"MaximumScale", capabilities.PageScalingFactorRange?.MaximumScale},
-                        {"MinimumScale", capabilities.PageScalingFactorRange?.MinimumScale},
+                    PageResolution = [.. capabilities.PageResolutionCapability.Select(c => new PageResolutionDto
+                    {
+                        Resolution = c.QualitativeResolution.ToString(),
+                        X = c.X.ToString(),
+                        Y = c.Y.ToString(),
+                    })],
+                    PageScalingFactorRange = new PageScalingFactorRangeDto
+                    {
+                        MaximumScale = capabilities.PageScalingFactorRange?.MaximumScale,
+                        MinimumScale = capabilities.PageScalingFactorRange?.MinimumScale,
                     },
                     PagesPerSheet = [.. capabilities.PagesPerSheetCapability.Select(c => c.ToString())],
                     PagesPerSheetDirection = [.. capabilities.PagesPerSheetDirectionCapability.Select(c => c.ToString())],
